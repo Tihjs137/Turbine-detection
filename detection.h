70afe2a4638c554d89b89cf5347cfd2f9fc4d6b3
Tihjs137 <thijsdejong21@gmail.com>
2019-05-15 13:08:17 +0200

    Put the opencv code in its own class

---
+++ detection.h
@@ -0,0 +1,197 @@
+#pragma once
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std; 
+using namespace cv; 
+
+class Detector 
+{ 
+    // Access specifier 
+    public: 
+  
+    // Data Members 
+    string name; 
+    //Parameters
+    
+    int  bw2_Treshold;    
+    int  bw4_Rho     ;    
+    int  bw4_Theta   ;   
+    int  bw4_Treshold;  
+    int  bw4_srn     ; 
+    int  bw4_stn     ;
+
+    Detector()
+    {
+        bw2_Treshold    = 180;
+        bw4_Rho         = 1;
+        bw4_Theta       = 180;
+        bw4_Treshold    = 80;
+        bw4_srn         = 30;
+        bw4_stn         = 10;
+
+        //create trackbars
+        namedWindow(    "bw2 mask",1);
+        createTrackbar( "bw2_Treshold",     "bw2 mask", &bw2_Treshold, 255, NULL );
+
+        namedWindow(    "bw4: Hough lines",1);
+        createTrackbar( "bw4_Rho",          "bw4: Hough lines", &bw4_Rho,       10, NULL );
+        createTrackbar( "bw4_Theta",        "bw4: Hough lines", &bw4_Theta,     360, NULL );
+        createTrackbar( "bw4_Treshold",     "bw4: Hough lines", &bw4_Treshold,  255, NULL );
+        createTrackbar( "bw4_srn",          "bw4: Hough lines", &bw4_srn,       60, NULL );
+        createTrackbar( "bw4_stn",          "bw4: Hough lines", &bw4_stn,       30, NULL );
+    };
+
+    // Member Functions() 
+    void printname() 
+    { 
+       cout << "Geekname is: " << name; 
+    } 
+
+    void showVideo(Mat frame)
+    {
+            
+        imshow("live",frame);
+
+    }
+
+    void detect(bool video, Mat frame)
+    {
+        
+
+        
+            
+        
+        // show live and wait for a key with timeout long enough to show images
+        imshow("Live", frame);
+
+        // -------- gray scaling ----------
+
+        Mat BW1;
+        cvtColor(frame, BW1, CV_BGR2GRAY);
+        imshow("bw1 grey", BW1);
+
+        // -------- Masking ----------
+        Mat BW2 = BW1 > bw2_Treshold;
+    
+        imshow("bw2 mask", BW2);
+
+
+        // -------- Skeleton ----------
+
+        Mat skel(BW2.size(), CV_8UC1, Scalar(0));
+        Mat temp;
+        Mat eroded;
+        Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
+        Mat BW3 = BW2;
+        bool done;
+        int iterations=0;
+
+        do
+        {
+        erode(BW3, eroded, element);
+        dilate(eroded, temp, element);
+        subtract(BW3, temp, temp);
+        bitwise_or(skel, temp, skel);
+        eroded.copyTo(BW3);
+
+        done = (countNonZero(BW3) == 0);
+        iterations++;
+        
+        } while (!done && (iterations < 100));
+
+        //cout << "iterations=" << iterations << endl;
+        
+        imshow("bw3: skeleton", skel);
+
+        //-------Hough lines----------//
+        //C++: void HoughLines(InputArray image, OutputArray lines, double rho, double theta, int threshold, double srn=0, double stn=0 )
+
+        Mat BW4 = frame;
+        vector<Vec4i> lines;
+        int Xmax = frame.size[0]/2;
+        int Xmin = frame.size[0]/2;
+        int Ymax = frame.size[1]/2;
+        int Ymin = frame.size[1]/2;
+
+        Rect boundingBox;
+
+        HoughLinesP( skel, lines, bw4_Rho, CV_PI/bw4_Theta, bw4_Treshold, bw4_srn, bw4_stn );
+        cout << "BW4 \t Found hough lines: \n";
+        for( size_t i = 0; i < lines.size(); i++ )
+        {
+            line( BW4, Point(lines[i][0], lines[i][1]),
+                Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 3, 8 );
+            
+            
+
+            //Create bounding box
+            if (Xmax < lines[i][0] || Xmax < lines[i][2] )
+            {
+                if( lines[i][0] > lines[i][2])
+                {
+                    Xmax = lines[i][0];
+                }
+                else
+                {
+                    Xmax = lines[i][2];
+                }
+            }
+            if (Xmin > lines[i][0] || Xmin > lines[i][2] )
+            {
+                if( lines[i][0] < lines[i][2])
+                {
+                    Xmin = lines[i][0];
+                }
+                else
+                {
+                    Xmin = lines[i][2];
+                }
+            }
+            if (Ymax < lines[i][1] || Ymax < lines[i][3] )
+            {
+                if( lines[i][1] > lines[i][3])
+                {
+                    Ymax = lines[i][1];
+                }
+                else
+                {
+                    Ymax = lines[i][3];
+                }
+            }
+            if (Ymin > lines[i][1] || Ymin > lines[i][3] )
+            {
+                if( lines[i][1] < lines[i][3])
+                {
+                    Ymin = lines[i][1];
+                }
+                else
+                {
+                    Ymin = lines[i][3];
+                }
+            }
+            
+            boundingBox.x       = Xmin;
+            boundingBox.y       = Ymin;
+            boundingBox.width   = Xmax - Xmin; 
+            boundingBox.height  = Ymax - Ymin;
+            
+            float  slope = atan( (float)( (int)lines[i][3] - (int)lines[i][1]) / (float)((int)lines[i][2] - (int)lines[i][0]) ) * (180 / 3.1415);
+
+
+            cout << "BW4 \t Line: " << i << "\t P1_x, P1_y: (" << lines[i][0] << "," << lines[i][1] << ") \t P2_x, P2_y: ";
+            cout << lines[i][2] << "," << lines[i][3]  << "\t Slope: " << slope << "\n";
+        }
+
+        cout << "BW4 \t box: " << 1 << "\t Xmin, Xmax: (" << Xmin << "," << Xmax << ") \t Ymin,Ymax: (" << Ymin << "," << Ymax  << ")\n";
+        rectangle(BW4, boundingBox, Scalar( 0, 255, 255 ));
+        imshow("bw4: Hough lines", BW4);
+        //Mat& img, Rect rec, const Scalar& color,
+
+    }    
+
+}; 
--- main.cpp
+++ main.cpp
@@ -5,21 +5,25 @@
   @email  thijsdejong21@gmail.com
   @date   14th of May, 2019
 */
-
+#pragma once
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
+#include "detection.h"
 
-using namespace cv;
-using namespace std;
+using namespace std; 
+using namespace cv; 
+
 
 
 
 int main(int argc, char** argv)
 {
+    Detector d;
+    
     Mat frame;
     VideoCapture cap;
     bool video;
@@ -36,14 +40,15 @@ int main(int argc, char** argv)
         // cap.open(0);
         // OR advance usage: select any API backend
 
-        int deviceID = 1;             // 0 = open default camera
+        int deviceID = 0;             // 0 = open default camera
         int apiID = cv::CAP_ANY;      // 0 = autodetect default API
 
         // open selected camera using selected API
         cap.open(deviceID + apiID);
 
         // check if we succeeded
-        if (!cap.isOpened()) {
+        if (!cap.isOpened()) 
+        {
             cerr << "ERROR! Unable to open camera\n";
             return -1;
         }
@@ -66,164 +71,30 @@ int main(int argc, char** argv)
         << "Press any key to terminate" << endl;
     
 
-    //Parameters
-    int bw2_Treshold = 180;
-    int  bw4_Rho         = 1;
-    int  bw4_Theta       = 180;
-    int  bw4_Treshold    = 80;
-    int  bw4_srn         = 30;
-    int  bw4_stn         = 10;
-
-    //create trackbars
-    namedWindow(    "bw2 mask",1);
-    createTrackbar( "bw2_Treshold",     "bw2 mask", &bw2_Treshold, 255, NULL );
-
-    namedWindow(    "bw4: Hough lines",1);
-    createTrackbar( "bw4_Rho",          "bw4: Hough lines", &bw4_Rho,       10, NULL );
-    createTrackbar( "bw4_Theta",        "bw4: Hough lines", &bw4_Theta,     360, NULL );
-    createTrackbar( "bw4_Treshold",     "bw4: Hough lines", &bw4_Treshold,  255, NULL );
-    createTrackbar( "bw4_srn",          "bw4: Hough lines", &bw4_srn,       60, NULL );
-    createTrackbar( "bw4_stn",          "bw4: Hough lines", &bw4_stn,       30, NULL );
-
-    for (;;)
+    // ======= Main Vision Loop =======
+    for(;;)
     {
-        if(video)
+        if(video) //1 frame or frame feed?
         {
             // wait for a new frame from camera and store it into 'frame'
             cap.read(frame);
             // check if we succeeded
-            if (frame.empty()) {
+            if (frame.empty()) 
+            {
                 cerr << "ERROR! blank frame grabbed\n";
                 break;
             }
         }
         
-        // show live and wait for a key with timeout long enough to show images
-        imshow("Live", frame);
-
-        // -------- gray scaling ----------
-
-        Mat BW1;
-        cvtColor(frame, BW1, CV_BGR2GRAY);
-        imshow("bw1 grey", BW1);
-
-        // -------- Masking ----------
-        Mat BW2 = BW1 > bw2_Treshold;
-       
-        imshow("bw2 mask", BW2);
-
-
-        // -------- Skeleton ----------
-
-        Mat skel(BW2.size(), CV_8UC1, Scalar(0));
-        Mat temp;
-        Mat eroded;
-        Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
-        Mat BW3 = BW2;
-        bool done;
-        int iterations=0;
-
-        do
-        {
-        erode(BW3, eroded, element);
-        dilate(eroded, temp, element);
-        subtract(BW3, temp, temp);
-        bitwise_or(skel, temp, skel);
-        eroded.copyTo(BW3);
-
-        done = (countNonZero(BW3) == 0);
-        iterations++;
         
-        } while (!done && (iterations < 100));
-
-        //cout << "iterations=" << iterations << endl;
         
-        imshow("bw3: skeleton", skel);
-
-        //-------Hough lines----------//
-        //C++: void HoughLines(InputArray image, OutputArray lines, double rho, double theta, int threshold, double srn=0, double stn=0 )
-
-        Mat BW4 = frame;
-        vector<Vec4i> lines;
-        int Xmax = frame.size[0]/2;
-        int Xmin = frame.size[0]/2;
-        int Ymax = frame.size[1]/2;
-        int Ymin = frame.size[1]/2;
-
-        Rect boundingBox;
-
-        HoughLinesP( skel, lines, bw4_Rho, CV_PI/bw4_Theta, bw4_Treshold, bw4_srn, bw4_stn );
-	    cout << "BW4 \t Found hough lines: \n";
-        for( size_t i = 0; i < lines.size(); i++ )
-        {
-            line( BW4, Point(lines[i][0], lines[i][1]),
-                Point(lines[i][2], lines[i][3]), Scalar(0,0,255), 3, 8 );
-            
-            
-
-            //Create bounding box
-            if (Xmax < lines[i][0] || Xmax < lines[i][2] )
-            {
-                if( lines[i][0] > lines[i][2])
-                {
-                    Xmax = lines[i][0];
-                }
-                else
-                {
-                    Xmax = lines[i][2];
-                }
-            }
-            if (Xmin > lines[i][0] || Xmin > lines[i][2] )
-            {
-                if( lines[i][0] < lines[i][2])
-                {
-                    Xmin = lines[i][0];
-                }
-                else
-                {
-                    Xmin = lines[i][2];
-                }
-            }
-            if (Ymax < lines[i][1] || Ymax < lines[i][3] )
-            {
-                if( lines[i][1] > lines[i][3])
-                {
-                    Ymax = lines[i][1];
-                }
-                else
-                {
-                    Ymax = lines[i][3];
-                }
-            }
-            if (Ymin > lines[i][1] || Ymin > lines[i][3] )
-            {
-                if( lines[i][1] < lines[i][3])
-                {
-                    Ymin = lines[i][1];
-                }
-                else
-                {
-                    Ymin = lines[i][3];
-                }
-            }
-            
-            boundingBox.x       = Xmin;
-            boundingBox.y       = Ymin;
-            boundingBox.width   = Xmax - Xmin; 
-            boundingBox.height  = Ymax - Ymin;
-            
-            float  slope = atan( (float)( (int)lines[i][3] - (int)lines[i][1]) / (float)((int)lines[i][2] - (int)lines[i][0]) ) * (180 / 3.1415);
-
-
-            cout << "BW4 \t Line: " << i << "\t P1_x, P1_y: (" << lines[i][0] << "," << lines[i][1] << ") \t P2_x, P2_y: ";
-            cout << lines[i][2] << "," << lines[i][3]  << "\t Slope: " << slope << "\n";
-        }
-
-        cout << "BW4 \t box: " << 1 << "\t Xmin, Xmax: (" << Xmin << "," << Xmax << ") \t Ymin,Ymax: (" << Ymin << "," << Ymax  << ")\n";
-        rectangle(BW4, boundingBox, Scalar( 0, 255, 255 ));
-        imshow("bw4: Hough lines", BW4);
-        //Mat& img, Rect rec, const Scalar& color,
         
+        
+
+        //Run the detector
+        d.detect(video, frame);
+        
+
         //-------Wait key -----------
         if(!video)
         {
@@ -238,9 +109,13 @@ int main(int argc, char** argv)
         }
 
         if (waitKey(5) >= 0)
-                    return 0;
+        {
+            return 0;
+        }    
+    }            
 
-    }
+    
     // the camera will be deinitialized automatically in VideoCapture destructor
+    
     return 0;
 }
