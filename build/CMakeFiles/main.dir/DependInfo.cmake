4bfc91721e0b9c8b429e40e3a2690fdf4f5a14ef
Tihjs137 <thijsdejong21@gmail.com>
2019-05-15 17:23:43 +0200

    Cmake on jNano

--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,14 +4,13 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/thijs/Documents/Minor/Project/cmake/houghlines/main.cpp" "/home/thijs/Documents/Minor/Project/cmake/houghlines/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/thijs/cmake/Turbine-detection/main.cpp" "/home/thijs/cmake/Turbine-detection/build/CMakeFiles/main.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
-  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/usr/include/opencv"
   )
 
 # Targets to which this target links.
